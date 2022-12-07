81d0750fd3ea65787087d9a7679d849202ee073a
Kevin <47770053+kevinwchen@users.noreply.github.com>
2022-12-07 12:44:25 +1100

    Implement verification with hardhat etherscan

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,13 +1,35 @@
-// Imports
-const { ethers } = require("hardhat")
+const { ethers, run, network } = require("hardhat")
 
-// async main
+// Main function to deploy contract
 async function main() {
   const SimpleStorageFactory = await ethers.getContractFactory("SimpleStorage")
   console.log("Deploying contract...")
   const simpleStorage = await SimpleStorageFactory.deploy()
   await simpleStorage.deployed()
   console.log(`Contract deployed to ${simpleStorage.address}`)
+
+  // Check this is deploying to Goerli
+  if (network.config.chainId === 5 && process.env.ETHERSCAN_API_KEY) {
+    await simpleStorage.deployTransaction.wait(6)
+    await verify(simpleStorage.address, [])
+  }
+}
+
+// Verify contract on etherscan
+async function verify(contractAddress, args) {
+  console.log("Verifying contract...")
+  try {
+    await run("verify:verify", {
+      address: contractAddress,
+      constructorArguments: args,
+    })
+  } catch (e) {
+    if (e.message.toLowerCase().includes("already verified")) {
+      console.log("Already Verified!")
+    } else {
+      console.log(e)
+    }
+  }
 }
 
 // main
